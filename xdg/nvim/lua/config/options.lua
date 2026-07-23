34d00f32faceeb4c73c216be267366a667c06ea3
Benjamin Secker <benjamin.secker@cdds.ai>
2026-07-23 15:46:47 +0200

    actually autosave without autoformatting

--- xdg/nvim/lua/config/options.lua
+++ xdg/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Keep explicit formatting available, but never format as part of a write/autosave.
+vim.g.autoformat = false
--- xdg/nvim/lua/plugins/disabled.lua
+++ xdg/nvim/lua/plugins/disabled.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    opts = {
-      format_on_save = false,
-    },
-  },
-  {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
