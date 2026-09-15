5c314f969484ad844786e81aa68c489ec239400a
Benjamin Secker <benjamin.secker@cdds.ai>
2026-09-15 13:25:11 +0200

    niri, nvim

--- xdg/nvim/lua/plugins/autosave.lua
+++ xdg/nvim/lua/plugins/autosave.lua
@@ -1,4 +1,10 @@
 return {
   "okuuva/auto-save.nvim",
-  opts = {},
+  opts = {
+    trigger_events = {
+      immediate_save = { "BufLeave", "FocusLost" },
+      defer_save = {},
+      cancel_deferred_save = {},
+    },
+  },
 }
